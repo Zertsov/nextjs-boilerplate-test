8f9265df30b2e5b8f2e071ab424ae5ea3657bb1f
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2025-02-06 17:32:16 -0600

    testing deploy for vercel sys env

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+console.log('NEXT CONFIG')
+Object.keys(process.env)
+  .filter(key => key.startsWith("VERCEL"))
+  .forEach(key => console.log(`${key}: ${process.env[key]}`));
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
