e23872254bf08e3bfb523168e7df52caedea36a6
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2025-02-13 14:50:41 -0600

    leggo

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   return (
     <>
-      <h1>Vite + React + L + Vercel URL: {vcURL}</h1>
+      <h1>Vite + React + Vercel URL: {vcURL}</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
