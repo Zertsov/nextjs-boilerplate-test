1b14170f53e31604ee0df31d4df84a7f87750156
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2025-02-16 20:48:30 -0500

    less env vars

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 console.log('NEXT CONFIG')
-for (const key of Object.keys(process.env).filter(key => key.startsWith("VERCEL") || key.startsWith("NEXT_PUBLIC"))) {
+for (const key of Object.keys(process.env).filter(key => key.includes("ENV"))) {
   console.log(`${key}: ${process.env[key]}`)
 }
 
