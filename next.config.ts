cf6523c415f53f29f4f1b1e836d4f9637e9f39f5
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2025-02-16 20:38:20 -0500

    show next public as well

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 console.log('NEXT CONFIG')
-for (const key of Object.keys(process.env).filter(key => key.startsWith("VERCEL"))) {
+for (const key of Object.keys(process.env).filter(key => key.startsWith("VERCEL") || key.startsWith("NEXT_PUBLIC"))) {
   console.log(`${key}: ${process.env[key]}`)
 }
 
