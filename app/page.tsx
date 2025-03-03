a876426540b45c2d1086aef157dac3cc332d8c81
Mitch (a.k.a Voz) <mitch.vostrez@vercel.com>
2025-03-03 10:26:44 -0600

    build

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,10 @@ import { useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(0)
   const testEnvVar = process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_ID;
-  const confirmedEnvVar = process.env.NEXT_PUBLIC_VERCEL_ENV;
 
   return (
     <>
       <h1>Testing env var: {testEnvVar}</h1>
-      <h1>Good env var: {confirmedEnvVar}</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
           count is {count}
