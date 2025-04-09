51f9626f68c5e0302c7039d577495f67d4dd0ba5
Mitch (a.k.a Voz) <mitchelvostrez@gmail.com>
2025-04-08 19:29:42 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 export default function Home() {
   const [count, setCount] = useState(0)
   const testEnvVar = process.env.NEXT_PUBLIC_VERCEL_DEPLOYMENT_ID;
+  console.log('here');
 
   return (
     <>
